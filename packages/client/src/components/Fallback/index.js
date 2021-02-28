f82926021bdd636f4a4ad6aa1ff8ab08e8f2bd09
sunny335 <ahmedsunny868@gmail.com>
2021-02-28 19:32:22 +0600

    feat: client pages routes

---
+++ packages/client/src/components/Fallback/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Fallback = () => {
+  return (
+    <div className="fallback-loader">
+      <img src="" alt="preloader" />
+    </div>
+  );
+};
+
+export default Fallback;
